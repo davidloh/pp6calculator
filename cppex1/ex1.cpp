6489e33b6178313d0443bd1026861bc97c8c6c17
David Loh <davidloh@hotmail.co.uk>
2014-11-03 15:19:06 +0000

    add ex1 and ex2

---
+++ cppex1/ex1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+  //This is a comment
+  /*This is a
+   multiline comment*/
+
+  std::cout << "Hello World!\n";
+
+  return 0;
+
+}
---
+++ cppex2/ex2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+
+  double a,b,c;
+
+  std::cout << "Enter the value of a:\n";
+  std::cin >> a;
+
+  std::cout << "Enter the value of b:\n";
+  std::cin >> b;
+
+  c = a * b;
+
+  std::cout << "The value of a*b is " << c << std::endl;
+
+  return 0;
+
+}
